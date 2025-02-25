3371d80015d06aff0cd6884143ffd00413653214
Korieuko <167052643+Celesth@users.noreply.github.com>
2025-02-25 10:23:18 +0530

    Update config.py

--- Src/config.py
+++ Src/config.py
@@ -1,1 +1,6 @@
 
+# config.py - Stores user-accessible settings
+
+URL = ""  # Set a default URL or leave blank to prompt user input
+TIME = "1"  # Change to 1h, 12h, 24h, or 72h
+BOOCAT_API = "YOUR_USER_WEBHOOK_HERE"
